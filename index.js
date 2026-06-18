edeca87ca068da18fb8ec4591c183852d5db7e81
Md-Romjan-Ali <mdromjanali224512@gmail.com>
2026-06-18 22:48:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const { setServers } = require("node:dns");
+setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require('express')
+const cors = require("cors")
+const app = express()
+require("dotenv").config()
+const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, Collection, ObjectId } = require('mongodb');
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.MONGODB_URI
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        await client.connect();
+        const db = client.db("Property-Rental");
+        const ownerCollection = db.collection('owner')
+        const clientCollection = db.collection('client')
+
+        app.post("/api/ownerpost", async (req, res) => {
+            const query = req.body;
+            const result = await ownerCollection.insertOne(query)
+            res.send(result)
+        })
+        app.get('/api/ownerpost', async (req, res) => {
+            const result = await ownerCollection.find().toArray()
+            res.send(result)
+        })
+        app.get('/api/ownerpost/:id', async (req, res) => {
+            const { id } = req.params
+            const query = { _id: new ObjectId(id) }
+            const result = await ownerCollection.findOne(query)
+            res.send(result)
+        })
+        // client says start
+        app.post('/api/clientsays', async (req, res) => {
+            const corsor = req.body
+            const reuslt = await clientCollection.insertOne(corsor)
+            res.send(reuslt)
+        })
+        app.get('/api/clientsays', async (req, res) => {
+            const result = await clientCollection.find().toArray()
+            res.send(result)
+        })
+        // client says end
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
